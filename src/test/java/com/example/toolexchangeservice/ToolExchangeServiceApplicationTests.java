46cd9585c0304e231c4eee7b75a66c5a593dd999
Marko Jerkić <markojerkic266@gmail.com>
2021-10-19 09:41:02 +0200

    initial commit

---
+++ src/test/java/com/example/toolexchangeservice/ToolExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toolexchangeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolExchangeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
