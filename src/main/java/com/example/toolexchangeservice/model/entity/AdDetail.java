0c2bf90bb57823e52e2d1f926884d50dfce5027d
Josip Vazdar <jv52429@fer.hr>
2021-11-01 16:26:14 +0100

    Proba

---
+++ src/main/java/com/example/toolexchangeservice/model/entity/AdDetail.java
@@ -0,0 +1,29 @@
+package com.example.toolexchangeservice.model.entity;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class AdDetail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column
+    @NotNull
+    private String title;
+    @Column
+    @NotNull
+    private String details;
+    @Column
+    @NotNull
+    private Date lastModified;
+    @Column
+    @NotNull
+    private UserDetail creator;
+}
---
+++ src/main/java/com/example/toolexchangeservice/repository/AdRepository.java
@@ -0,0 +1,14 @@
+package com.example.toolexchangeservice.repository;
+
+import com.example.toolexchangeservice.model.entity.AdDetail;
+import com.example.toolexchangeservice.model.entity.UserDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AdRepository extends JpaRepository<AdDetail, Long> {
+    Optional<UserDetail> findByTitle(String username);
+    boolean existsByTitle(String username);
+
+
+}
