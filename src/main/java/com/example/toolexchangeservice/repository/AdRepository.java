aeabd8eb9ee8c9de9eef184b7f110ccac4230293
Josip Vazdar <jv52429@fer.hr>
2021-11-04 11:13:23 +0100

    Promjena naziva
    Promjenjen naziv stringa za pretragu

--- src/main/java/com/example/toolexchangeservice/repository/AdRepository.java
+++ src/main/java/com/example/toolexchangeservice/repository/AdRepository.java
@@ -7,8 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface AdRepository extends JpaRepository<AdDetail, Long> {
-    Optional<UserDetail> findByTitle(String username);
-    boolean existsByTitle(String username);
-
-
+    Optional<UserDetail> findByTitle(String title);
+    boolean existsByTitle(String title);
 }
